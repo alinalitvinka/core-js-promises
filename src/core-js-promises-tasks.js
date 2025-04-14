4c7d738f92f413ea6544f610b5cc68d4ba901c3c
alinalitvinka <alinalitvinko7@gmail.com>
2025-04-14 15:33:59 +0300

    feat: solve 1st task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,16 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  const promise = new Promise((resolve, reject) => {
+    if (number >= 0) {
+      resolve(this.response);
+    } else {
+      const error = new Error('Number is negative!');
+      reject(error);
+    }
+  });
+  return promise;
 }
 
 /**
